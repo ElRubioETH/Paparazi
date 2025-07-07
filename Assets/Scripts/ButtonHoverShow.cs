ac6fa54d17942504f5560b942de2964959f2c437
thai1349 <hongthai98447@gmail.com>
2025-07-07 18:17:22 +0700

    acn

---
+++ Assets/Scripts/ButtonHoverShow.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonHoverShow : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public RawImage targetImage; // RawImage bạn muốn hiển thị khi hover
+
+    void Start()
+    {
+        if (targetImage != null)
+        {
+            targetImage.gameObject.SetActive(false); // Ẩn lúc đầu
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (targetImage != null)
+        {
+            targetImage.gameObject.SetActive(true); // Hiện khi hover
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (targetImage != null)
+        {
+            targetImage.gameObject.SetActive(false); // Ẩn khi rời chuột
+        }
+    }
+}
