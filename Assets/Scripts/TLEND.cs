47bb164c0a903a79bb75485d1d2e110a8854b69c
ElRubioETH <henkenmanybruh@gmail.com>
2025-07-09 20:32:49 +0700

    hehe

---
+++ Assets/Scripts/TLEND.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+
+public class TimelineController : MonoBehaviour
+{
+    public PlayableDirector director;
+
+    void Start()
+    {
+        // Tự chạy timeline khi start
+        // PlayTimeline();
+    }
+
+    public void PlayTimeline()
+    {
+        if (director != null)
+        {
+            director.Play();
+        }
+    }
+}
