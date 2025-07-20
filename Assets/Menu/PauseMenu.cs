3f428610ec37d3ee8615ed14c5188d8fa0e6bd2c
ElRubioETH <henkenmanybruh@gmail.com>
2025-07-21 03:59:13 +0700

    hehe

--- Assets/Menu/PauseMenu.cs
+++ Assets/Menu/PauseMenu.cs
@@ -23,6 +23,8 @@ public class PauseMenu : MonoBehaviour
         pausePanel.SetActive(true);
         Time.timeScale = 0f;
         isPaused = true;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
     }
 
     public void Resume()
@@ -30,6 +32,8 @@ public class PauseMenu : MonoBehaviour
         pausePanel.SetActive(false);
         Time.timeScale = 1f;
         isPaused = false;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     public void QuitGame()
