088c8d4088d50d8393306faefe80b3159d439258
ElRubioETH <henkenmanybruh@gmail.com>
2025-07-23 14:52:25 +0700

    hehe

--- Assets/Model/Teddy/Hint.cs
+++ Assets/Model/Teddy/Hint.cs
@@ -1,16 +1,28 @@
+﻿using TMPro;
 using UnityEngine;
 
 public class Hint : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public TextMeshProUGUI dialogueText; // Gắn từ Inspector
+
+    public void Show(string message)
     {
-        
+        if (dialogueText != null)
+        {
+            dialogueText.text = message;
+            dialogueText.gameObject.SetActive(true);
+        }
+        else
+        {
+            Debug.LogWarning("Dialogue Text is not assigned!");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Hide()
     {
-        
+        if (dialogueText != null)
+        {
+            dialogueText.gameObject.SetActive(false);
+        }
     }
 }
