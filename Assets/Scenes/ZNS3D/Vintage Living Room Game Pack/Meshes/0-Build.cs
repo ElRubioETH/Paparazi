c372fbf59e9210a699e9c1f492cb888e20f2c4f3
ElRubioETH <henkenmanybruh@gmail.com>
2025-07-28 14:32:55 +0700

    4

---
+++ Assets/Scenes/ZNS3D/Vintage Living Room Game Pack/Meshes/0-Build.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class FloorBuilder : MonoBehaviour
+{
+    public GameObject floorPrefab;
+    public int width = 3;
+    public int height = 3;
+    public float tileSize = 1f;
+
+    void Start()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int z = 0; z < height; z++)
+            {
+                Vector3 spawnPos = new Vector3(x * tileSize, 0, z * tileSize);
+                Instantiate(floorPrefab, spawnPos, Quaternion.identity, transform);
+            }
+        }
+    }
+}
